1556e293aed3caae124ad1cfb5b52a3b6ad0fa61
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-02-01 00:00:04 -0300

    feat: add button component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Button {...args} />
+
+Basic.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}
