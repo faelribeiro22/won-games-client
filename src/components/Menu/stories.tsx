75aba75596ffe3c8b47fdfcbf18d215043c76b38
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-02-03 22:56:38 -0300

    feat: add menu desktop and mobile

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Menu, { MenuProps } from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Basic: Story<MenuProps> = (args) => <Menu {...args} />
+
+Basic.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
