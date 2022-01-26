4bf7daa02ef6d207179dd03fd1c6fd77d5e52301
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2022-01-26 01:30:24 -0300

    feat: add highlight background image

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -5,6 +5,7 @@ import Highlight from '.'
 const props = {
   title: 'Heading 1',
   subtitle: 'Heading 2',
+  backgroundImage: '/img/red-dead-background.jpg',
   buttonLabel: 'Buy now',
   buttonLink: '/rdr2'
 }
@@ -22,5 +23,13 @@ describe('<Highlight />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
   })
 })
