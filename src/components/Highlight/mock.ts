4a8095419dc23a0c8957c5d6bbf25eeec02afca8
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2022-04-27 01:23:58 -0300

    Feature/home (#734)
    * build(deps): bump next from 12.1.2 to 12.1.4
    
    Bumps [next](https://github.com/vercel/next.js) from 12.1.2 to 12.1.4.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v12.1.2...v12.1.4)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * finish home with mocks
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: `Read Dead it's back`,
+  subtitle: `It's a long story`,
+  buttonLabel: 'Buy now',
+  backgroundImage: '/img/red-dead-background.jpg',
+  buttonLink: '/rdr2'
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Main from 'components/Main'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Home() {
-  return <Main />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
+    }
+  }
 }
