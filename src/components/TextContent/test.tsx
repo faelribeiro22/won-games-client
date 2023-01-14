81794e869ff7ce9423758eefc353c4cd792d0b14
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2023-01-14 12:54:01 -0300

    feat: create textContent

---
+++ src/components/TextContent/test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import TextContent from '.'
+
+describe('<TextContent />', () => {
+  it('should render the heading', () => {
+    expect(true).toBe(true)
+  })
+})
